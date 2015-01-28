6a489df23f98b832502207aa62cc5da969f31802
Dave Tapley <dukedave@gmail.com>
2015-01-28 14:39:51 -0700

    Move targetURL alias to translation model. target is a poor choice of attr name, see: http://emberjs.com/api/classes/Ember.Controller.html#property_target

--- app/controllers/translation.js
+++ app/controllers/translation.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-  targetURL: function() {
-    return this.get('model.target');
-  }.property('model.target'),
 });
--- app/models/translation.js
+++ app/models/translation.js
@@ -3,9 +3,10 @@ import DS from 'ember-data';
 
 export default DS.ModelFragment.extend({
   priority: DS.attr('number'),
-  target: DS.attr(),
+  target: DS.attr('string'),
   active: DS.attr('boolean'),
   rule: DS.hasOneFragment('rule', { polymorphic: true, typeKey: '$type' }),
 
-  hasRule: Ember.computed.bool('rule')
+  hasRule: Ember.computed.bool('rule'),
+  targetURL: Ember.computed.alias('target')
 });
