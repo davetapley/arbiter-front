533751c1d4f0116f90ff5ef6d448a5b33beb177e
Dave Tapley <dukedave@gmail.com>
2015-01-28 14:55:30 -0700

    Allow existing translation's rule to be changed

--- app/controllers/translation.js
+++ app/controllers/translation.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  actions: {
+    changeRule: function() {
+      this.set('rule', null);
+    },
+
+    addRule: function(type) {
+      this.set('rule', this.store.createFragment(type));
+    },
+  }
 });
