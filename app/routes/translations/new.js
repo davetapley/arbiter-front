80f2f759b6c1ffe56a1787a1999feaec3ee10bcc
Dave Tapley <dukedave@gmail.com>
2015-01-29 12:16:18 -0700

    Use existing controller's actions instead of redefining them

--- app/routes/translations/new.js
+++ app/routes/translations/new.js
@@ -1,20 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  controllerName: 'translation',
+
   model: function() {
     return this.store.createFragment('translation', {
     });
   },
 
   actions: {
-    changeRule: function() {
-      var newTranslation = this.controller.get('model');
-      newTranslation.set('rule', null);
-    },
-    addRule: function(type) {
-      var newTranslation = this.controller.get('model');
-      newTranslation.set('rule', this.store.createFragment(type));
-    },
     add: function() {
       var newTranslation = this.controller.get('model');
       var translations = this.controllerFor('translations').get('model');
