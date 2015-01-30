f873c434dd7ee34935d5dbfc2308eb9ad526f0bb
Dave Tapley <dukedave@gmail.com>
2015-01-30 11:12:08 -0700

    Active not needed since bc44a6609a24b7ca9189519987b20a4f133e7cf3

--- app/models/translation.js
+++ app/models/translation.js
@@ -4,7 +4,6 @@ import DS from 'ember-data';
 export default DS.ModelFragment.extend({
   priority: DS.attr('number'),
   target: DS.attr('string'),
-  active: DS.attr('boolean'),
   rule: DS.hasOneFragment('rule', { polymorphic: true, typeKey: '$type' }),
 
   hasRule: Ember.computed.bool('rule'),
