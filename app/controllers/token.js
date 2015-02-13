bd6ab4908355bd5a0bf528ed2023b7bd7bda05a4
Dave Tapley <dukedave@gmail.com>
2015-02-13 15:50:38 -0700

    Show a token's active translation target

--- app/controllers/token.js
+++ app/controllers/token.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ObjectController.extend({
+  href: function() {
+    return '/__/proxy/api/' + this.get('id');
+  }.property('id'),
+
+  activeTarget: Ember.computed.alias('activeTranslation.target'),
+
   actions: {
     save: function() {
       this.get('model').save();
--- app/models/token.js
+++ app/models/token.js
@@ -1,5 +1,12 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   translations: DS.hasManyFragments('translation', {defaultValue: []}),
+
+  activeTranslations: Ember.computed.filterBy('translations', 'rule.active', true),
+
+  activeTranslation: function() {
+    return this.get('activeTranslations').objectAt(0);
+  }.property('activeTranslations.@each')
 });
