2f5c86823db273e27bdf3a718fd939d55d831cc4
Dave Tapley <dukedave@gmail.com>
2015-02-15 23:32:35 -0700

    Token IDs now have a domain and path component

--- app/models/token.js
+++ app/models/token.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  domain: function() {
+    var id = this.get('id');
+    return id.substring(0, id.lastIndexOf(","));
+  }.property('id'),
+
+  path: function() {
+    var id = this.get('id');
+    return id.substring(id.lastIndexOf(",") + 1);
+  }.property('id'),
+
   translations: DS.hasManyFragments('translation', {defaultValue: []}),
 
   activeTranslations: Ember.computed.filterBy('translations', 'rule.active', true),
