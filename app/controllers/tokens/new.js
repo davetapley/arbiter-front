537bf42c24608b5801908b2a8fe33b7a67989699
Dave Tapley <dukedave@gmail.com>
2015-02-16 22:41:54 -0700

    Choose default or user domain

--- app/controllers/tokens/new.js
+++ app/controllers/tokens/new.js
@@ -1,6 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  domainType: 'default',
+
+  isDefaultDomain: Ember.computed.equal('domainType', 'default'),
+  isUserDomain: Ember.computed.equal('domainType', 'user'),
+
+  domainOptions: function() {
+    return this.store.findAll('domain');
+  }.property(),
+
+  defaultDomainOptions: Ember.computed.filterBy('domainOptions', 'type', 'default'),
+  userDomainOptions: Ember.computed.filterBy('domainOptions', 'type', 'user'),
+
+  userHasDomains: function() {
+    return userDomainOptions.length() > 0;
+  }.property('userDomainOptions'),
+
   actions: {
     submit: function() {
       var token = this.get('model');
